e8cc252ba4a6d36b1747b94ab6e3254073229af7
ywangdev <yaow888a@gmail.com>
2025-08-21 10:20:55 +0100

    update

---
+++ day7/challenge.py
@@ -0,0 +1,40 @@
+"""
+Question:
+
+Print the nodes in a binary tree level-wise. For example, the following should print 1, 2, 3, 4, 5.
+
+  1
+ / \
+2   3
+   / \
+  4   5
+"""
+
+from collections import deque
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+
+def solution(root):
+    if not root:
+        return []
+
+    result = []
+    queue = deque([root])
+
+    while queue:
+        node = queue.popleft()
+        result.append(node.val)
+
+        if node.left:
+            queue.append(node.left)
+
+        if node.right:
+            queue.append(node.right)
+
+    return result
